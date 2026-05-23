2ac856109e017f9248290ba77a9f82300f515cc1
malifsyahiir4-spec <m.alifsyahiir4@gmail.com>
2026-05-23 15:10:31 +0700

    push tampilan dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,4 @@ Route::get('/', function () {
 
 Route::resource('categories', CategoryController::class);
 Route::resource('questions', QuestionController::class);
+Route::get('/dashboard', [DashboardController::class, 'index']);
