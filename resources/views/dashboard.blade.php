99e25e711701e217963e9ed97836844f6970587c
malifsyahiir4-spec <m.alifsyahiir4@gmail.com>
2026-05-24 22:15:01 +0700

    push style css

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,8 @@
 <link rel="stylesheet"
 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
 <style>
 
 body{
